787f37c994adaeced2a37ebe61ce51f019381604
Waseem Aziz <Waseemaziz106@gmail.com>
2025-02-20 01:25:33 +0500

    all good

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import cookieParser from "cookie-parser";
 
 const app=express();
 app.use(cors({
-    origin: 'https://waseem106.github.io',
-    optionsSuccessStatus: 200 // For legacy browser support    
-
+    // origin: 'https://waseem106.github.io',
+    // optionsSuccessStatus: 200 // For legacy browser support    
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
  }))
-//  origin:process.env.CORS_ORIGIN,
- // credentials:true
+ 
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded())
